262b0821001cac46e061082ccc3279f43b33ff6a
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-02-18 23:20:34 +0800

    feat(lsp): add lua server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("kawaii.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "lua" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
