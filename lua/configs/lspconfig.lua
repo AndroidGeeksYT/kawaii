85f095f0c780a5137652955453a141cc3e40db0f
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-02-19 23:13:28 +0800

    refactor(lsp): reset to default

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("kawaii.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua" }
+local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
