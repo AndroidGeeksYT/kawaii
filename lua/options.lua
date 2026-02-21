864105ede650793d0fa988ff851872bea079584f
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-02-21 18:26:52 +0800

    refactor(options): add some examples

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,15 @@ require "kawaii.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- This are some examples uncomment if you want to use them.
+
+-- Indenting
+-- o.expandtab = true
+-- o.shiftwidth = 4
+-- o.smartindent = true
+-- o.tabstop = 4
+-- o.softtabstop = 4
+
+-- Wrap
+-- o.wrap = false
