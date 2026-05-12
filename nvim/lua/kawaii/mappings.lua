87f03186aeede56694cdd0d6adab9e333582c0a0
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-12 20:49:09 +0800

    feat(mappings): quit / exit using ctrl + n + q

--- nvim/lua/kawaii/mappings.lua
+++ nvim/lua/kawaii/mappings.lua
@@ -5,3 +5,6 @@ map("n", "<C-n>s", "<cmd>:w<CR>", { desc = "Save File" })
 
 -- File Explorer
 map("n", "<C-n>ex", "<cmd>:Ex<CR>", { desc = "File Explorer" })
+
+-- Quit / Exit
+map("n", "<C-n>q", "<cmd>:q<CR>", { desc = "Quit / Exit" })
