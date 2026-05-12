e842cb7e6caa8c53300e6b0294e97b0ba4d34cf6
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-05-12 20:44:18 +0800

    fix(mappings): change desc of netrw file explorer

--- nvim/lua/kawaii/mappings.lua
+++ nvim/lua/kawaii/mappings.lua
@@ -4,4 +4,4 @@ local map = vim.keymap.set
 map("n", "<C-n>s", "<cmd>:w<CR>", { desc = "Save File" })
 
 -- File Explorer
-map("n", "<C-n>ex", "<cmd>:Ex<CR>", { desc = "Save File" })
+map("n", "<C-n>ex", "<cmd>:Ex<CR>", { desc = "File Explorer" })
